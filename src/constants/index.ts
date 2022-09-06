4b74985a28343a7896438afa33f872b403326929
Serhii Stanislav <serhiistanislav@gmail.com>
2022-09-06 18:22:28 +0300

    adds some changes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
 // @ts-nocheck
 
 // Current DB (don't change or remove) *
-export const currentDB = 'localhost'; // *
-// export const currentDB = 'heroku'; // *
+// export const currentDB = 'localhost'; // *
+export const currentDB = 'heroku'; // *
 
 export const URI =
   'postgres://pmuduyjxapveez:d875bd3154345450eda5eab5f83a3164c5c766f14f5674e4c168e2ce062e7c32@ec2-52-51-3-22.eu-west-1.compute.amazonaws.com:5432/dbfn33l6ik38n2';
